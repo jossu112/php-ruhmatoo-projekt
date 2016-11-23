f0a0d6de91b823a6bbec6d3ece1d511114723584
Jan Treiberg <jantreiberg@users.noreply.github.com>
2016-11-23 08:56:42 +0200

    Added login and signup

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sisselogimise lehekülg</title>
+	</head>
+	<body>
+
+		<h1>Logi sisse</h1>
+		
+		<form method="POST">
+		
+			<label>E-post</label><br>
+			<input name="loginEmail" type ="email" placeholder="E-mail">
+		
+			<br> <br>
+			
+			<input name="loginPassword" type ="password" placeholder="Parool">
+			
+			<br> <br>
+			
+			<input type="submit" value="Logi sisse">
+		</form>
+
+	</body>
+</html>

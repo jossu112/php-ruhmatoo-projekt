33934b7f943259f6d1e75d1dfa76e40ab352df3d
Kristjan Kand <kkkand@users.noreply.github.com>
2016-12-07 09:41:24 +0200

    simple bootstrap for image

--- page/ratingpage.php
+++ page/ratingpage.php
@@ -44,3 +44,30 @@
 
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="js/like-dislike.js"></script>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Example of Bootstrap 3 Responsive Images</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<style type="text/css">
+    .box{
+        width: 50%;
+        border: 4px solid #000;
+    }
+	.bs-example{
+    	margin: 20px;
+    }
+</style>
+</head> 
+
+</body>
+</html>                                		
